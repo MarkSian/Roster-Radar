d1bc9aa1173eb517c40e6672c26216f59857a098
MarkSian <merksianipar@gmail.com>
2024-12-17 11:14:30 -0700

    Dual Panel Buttons
    Buttons w/ bootstrap
    added svg to said buttons
    Placed props and created interface for the props
    Will style more for on-hover but this should be good enough for the mvp
    Buttons are clickable and will respond in the console.

--- roster-radar-frontend/src/App.tsx
+++ roster-radar-frontend/src/App.tsx
@@ -4,12 +4,31 @@ import FooterSection from './components/FooterSection'
 import './App.css'
 
 function App() {
-  
+  const handleMyRosterClick = () => {
+    console.log('My Roster clicked');
+  };
 
+  const handleOtherRostersClick = () => {
+    console.log('Other Rosters clicked');
+  };
+
+  const handlePlayersClick = () => {
+    console.log('Players clicked');
+  };
+
+  const handleProRostersClick = () => {
+    console.log('Pro Rosters clicked');
+  };
+  
   return (
     <>
       <NavBar />
-      <DoublePanel />
+      <DoublePanel  
+        onMyRosterClick={handleMyRosterClick}
+        onOtherRostersClick={handleOtherRostersClick}
+        onPlayersClick={handlePlayersClick}
+        onProRostersClick={handleProRostersClick}
+        />
       <FooterSection />
     </>
   )
